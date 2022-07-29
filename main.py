3f61e70ef22da0f1facb88a13bfbeb2c526f9e4a
aarush1023 <86903630+FabricMcGaming@users.noreply.github.com>
2022-07-29 13:09:08 -0700

    comments

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import *
 
 
+# creates flask app
 app = Flask('Covidtracker')
 
+#decorator to say: this route will return this function
 @app.route('/')
 def main():
     return render_template('main.html')
 
 
+# runs app
 app.run()
