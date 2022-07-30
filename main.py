c40ca4935746635f297798ebc641483b2f4bf334
aarush1023 <86903630+FabricMcGaming@users.noreply.github.com>
2022-07-30 14:16:57 -0700

    parsehub scraping

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from flask import *
+import requests
+import json
 
+API_KEY = "tLnJZ1kb5BRj"
+PROJECT_TOKEN = "tu8tS2QaOuwJ"
+RUN_TOKEN = "tUyEkiT3xuD"
 
-# creates flask app
-app = Flask('Covidtracker')
-
-#decorator to say: this route will return this function
-@app.route('/')
-def main():
-    return render_template('main.html')
-
-
-# runs app
-app.run()
+response = requests.get(f'https://www.parsehub.com/api/v2/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key":API_KEY})
+print(response.text)
+data = json.loads(response.text)
+print(data)
