e0e46013e7d4da97a16983b5e798f9410da910ca
aarush1023 <86903630+FabricMcGaming@users.noreply.github.com>
2022-07-31 10:32:27 -0700

    finished
    finished

--- main.py
+++ main.py
@@ -5,24 +5,29 @@ import json
 app = Flask(__name__)
 
 
-url = "https://covid-19-tracking.p.rapidapi.com/v1/usa"
+url = "https://covid-19-tracking.p.rapidapi.com/v1/"
 
 headers = {
 	"X-RapidAPI-Key": "e04e892d93msh8f59f3d316df947p12c47cjsn8d90c3cafc8e",
 	"X-RapidAPI-Host": "covid-19-tracking.p.rapidapi.com"
 }
 
-response = requests.request("GET", url, headers=headers)
-response = dict(response.json())
-print(response)
+json_file = open('countries.json')
+countries = json.load(json_file)
+countrylist = []
+for i in countries['countries']:
+    countrylist.append(i['name'].lower())
 
 
 @app.route('/')
 def home():
-    response = requests.request("GET", url, headers=headers)
-    response = dict(response.json())
-    print(response['New Cases_text'])
-    return render_template('home.html', response=response)
+    responses = []
+    for country in countrylist:
+        response = requests.request("GET", url+country, headers=headers)
+        response = dict(response.json())
+        responses.append(response)
+    print(responses)
+    return render_template('home.html', responses=responses)
 
 
 if __name__ == '__main__':
